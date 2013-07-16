d2af786967fe59a812cc7f5f769c43d763328337
Brandon Kowalski <BrandonKowalski@kwall.org>
2013-07-16 10:28:14 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var loadVine = function() {
     url: 'http://dev.univfeed.com/api/schools',
     dataType: 'jsonp',
     success: function(data){
+      console.log(JSON.stringify(data));
       return data;
     },
     error: function(xhr, type){
