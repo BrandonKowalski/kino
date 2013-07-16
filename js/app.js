23326bc8348a4afc5a85ee975ce454c0472ffad0
Brandon Kowalski <BrandonKowalski@kwall.org>
2013-07-16 10:21:42 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
-$.ajax({
-  type: 'GET',
-  url: 'http://dev.univfeed.com/api/schools?callback=test',
-  // data to be added to query string:
-  // type of data we are expecting in return:
-  dataType: 'json',
-  timeout: 300,
-  context: $('body'),
-  success: function(data){
-    // Supposing this JSON payload was received:
-    //   {"project": {"id": 42, "html": "<div>..." }}
-    // append the HTML to context object.
-    alert(data);
-  },
-  error: function(xhr, type){
-    alert('Ajax error!')
-  }
-})
+var loadVine = function() {  
+  $.ajax({
+    type: 'GET',
+    url: 'http://dev.univfeed.com/api/schools?callback=test',
+    dataType: 'json',
+    success: function(data){
+      alert(data);
+    },
+    error: function(xhr, type){
+      alert('Ajax error!')
+    }
+  });
+}
