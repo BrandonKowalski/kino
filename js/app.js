80ee76976aea5c6501b1936a597a4215aee0f753
Brandon Kowalski <BrandonKowalski@kwall.org>
2013-07-16 10:33:49 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var v = [];
 var loadVine = function() {  
   $.ajax({
     type: 'GET',
-    url: 'http://dev.univfeed.com/api/schools',
+    url: "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fvine.co%2Fv%2FhWP0FJKKBvK%22%20and%20xpath%3D'%2Fhtml%2Fhead%2Fmeta'&format=json&diagnostics=true&callback=cbfunc",
     dataType: 'jsonp',
     success: function(data){
       v.push(data);
